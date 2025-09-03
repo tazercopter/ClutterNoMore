8e5258c57270fdb80fd624c349835dba3b4cca1e
Tazer <yaseenelgamal20094@gmail.com>
2025-09-03 23:12:21 +0300

    fixed vertical slab consumption

--- src/main/java/dev/tazer/clutternomore/common/mixin/BlockItemMixin.java
+++ src/main/java/dev/tazer/clutternomore/common/mixin/BlockItemMixin.java
@@ -1,6 +1,7 @@
 package dev.tazer.clutternomore.common.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import dev.tazer.clutternomore.common.blocks.VerticalSlabBlock;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
@@ -12,14 +13,27 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.Optional;
+
 import static dev.tazer.clutternomore.common.event.DatamapHandler.INVERSE_SHAPES_DATAMAP;
 
 @Mixin(BlockItem.class)
 public class BlockItemMixin {
     @Redirect(method = "place", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;consume(ILnet/minecraft/world/entity/LivingEntity;)V"))
     private void place(ItemStack instance, int amount, LivingEntity entity, @Local(argsOnly = true) BlockPlaceContext context, @Local(ordinal = 0) BlockState blockstate) {
-        if (blockstate.getOptionalValue(SlabBlock.TYPE).isEmpty() ||  blockstate.getValue(SlabBlock.TYPE) != SlabType.DOUBLE || !INVERSE_SHAPES_DATAMAP.containsKey(context.getItemInHand().getItem())) {
-            instance.consume(amount, entity);
+        boolean consume = false;
+        if (INVERSE_SHAPES_DATAMAP.containsKey(context.getItemInHand().getItem())) {
+            Optional<SlabType> slabType = blockstate.getOptionalValue(SlabBlock.TYPE);
+            Optional<Boolean> isDouble = blockstate.getOptionalValue(VerticalSlabBlock.DOUBLE);
+
+            if (slabType.isEmpty() && isDouble.isEmpty()) {
+                consume = true;
+            } else {
+                if (slabType.isPresent()) consume = slabType.get() != SlabType.DOUBLE;
+                if (isDouble.isPresent()) consume = isDouble.get();
+            }
         }
+
+        if (consume) instance.consume(amount, entity);
     }
 }
